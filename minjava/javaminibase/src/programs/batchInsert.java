7864a45fa4d17c2b86317d42895a122df54e9e41
Jonathan <jonreyes4@gmail.com>
2020-03-09 20:56:25 -0700

    Index pseudo code based off of indexTest.java

--- minjava/javaminibase/src/programs/batchInsert.java
+++ minjava/javaminibase/src/programs/batchInsert.java
@@ -10,6 +10,7 @@ public class batchInsert{
         String datafilename = args[0];
         int type = Integer.parseInt(args[1]);
         String bigTableName = args[2];
+        //TODO System Defs
         //SystemDefsBigDB sysDefs = new SystemDefsBigDB();
         BufferedReader br = null;
         String line = "";
@@ -23,6 +24,33 @@ public class batchInsert{
                 String timeStamp = map[2];
                 String value = map[3];
                 System.out.println("row label: "+ rowLabel + " col label: " + columnLabel + " TS: " + timeStamp  + " val: " + value);
+
+                //TODO Map
+
+                //TODO insert into big table
+
+                //TODO take big table, scan it and index based on type
+
+                /*TODO
+                * switch(type):
+                * case 1:
+                * case 2: BTREE RowLabel
+                *       BTreeFile btf = null;
+                         btf = new BTreeFile("Name of the btree index file", AttrType.attrString, REC_LEN1   , 1/*delete);
+                         mid = new MID();
+                        String key = null;
+                        Tuple temp = null;
+                         while ( temp != null) {
+                            m.tupleCopy(temp);
+	                         key = m.getrowlabel();
+      }
+
+                 Case 3: BTREE Column label
+                        ...
+                 	     key = m.getcolumnlabel();
+
+                 */
+
 
             }
         } catch (FileNotFoundException e) {
