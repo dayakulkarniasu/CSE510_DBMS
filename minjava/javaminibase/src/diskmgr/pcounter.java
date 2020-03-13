f47b867e10fd84c674402953c2fc9e37f0ba862c
alwaysmiler <45401427+alwaysmiler@users.noreply.github.com>
2020-03-12 22:13:21 -0700

    add pcounter class to count read and write

---
+++ minjava/javaminibase/src/diskmgr/pcounter.java
@@ -0,0 +1,24 @@
+/* File pcounter.java */
+
+package diskmgr;
+
+import java.io.*;
+import bufmgr.*;
+import global.*;
+
+public class PCounter implements GlobalConst {
+
+  public static int rcounter;
+  public static int wcounter;
+  public static void initialize(){
+	  rcounter=0;
+	  wcounter=0;
+  }
+  public static void readIncrement(){
+	 rcounter++; 
+  }
+  public static void writeIncrement(){
+	 wcounter++; 
+  }
+  
+}//end of DB class
