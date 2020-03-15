ca1824c8df11bdcdb2ea5483f486c0726d9c1612
ShunchiZhou <excsimonzsc@gmail.com>
2020-03-15 14:32:43 -0700

    trivial changes

--- minjava/javaminibase/src/btree/LeafData.java
+++ minjava/javaminibase/src/btree/LeafData.java
@@ -1,30 +1,42 @@
 package btree;
+
 import global.*;
 
-/**  IndexData: It extends the DataClass.
- *   It defines the data "mid" for leaf node in B++ tree.
+/**
+ * IndexData: It extends the DataClass. It defines the data "mid" for leaf node
+ * in B++ tree.
  */
 public class LeafData extends DataClass {
   private MID myMid;
 
   public String toString() {
-     String s;
-     s="[ "+ (new Integer(myMid.pageNo.pid)).toString() +" "
-              + (new Integer(myMid.slotNo)).toString() + " ]";
-     return s;
+    String s;
+    s = "[ " + (new Integer(myMid.pageNo.pid)).toString() + " " + (new Integer(myMid.slotNo)).toString() + " ]";
+    return s;
   }
 
-  /** Class constructor
-   *  @param    mid  the data mid
+  /**
+   * Class constructor
+   * 
+   * @param mid the data mid
    */
-  LeafData(MID mid) {myMid= new MID(mid.pageNo, mid.slotNo);};  
+  LeafData(MID mid) {
+    myMid = new MID(mid.pageNo, mid.slotNo);
+  };
 
-  /** get a copy of the mid
-  *  @return the reference of the copy 
-  */
-  public MID getData() {return new MID(myMid.pageNo, myMid.slotNo);};
+  /**
+   * get a copy of the mid
+   * 
+   * @return the reference of the copy
+   */
+  public MID getData() {
+    return new MID(myMid.pageNo, myMid.slotNo);
+  };
 
-  /** set the rid
-   */ 
-  public void setData(MID mid) { myMid= new MID(mid.pageNo, mid.slotNo);};
-}   
+  /**
+   * set the mid
+   */
+  public void setData(MID mid) {
+    myMid = new MID(mid.pageNo, mid.slotNo);
+  };
+}
