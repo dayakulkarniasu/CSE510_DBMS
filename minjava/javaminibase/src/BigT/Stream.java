b9725155e5e6cd12e9777134fcd9e75d13b43d68
Jonathan <jonreyes4@gmail.com>
2020-03-19 22:06:01 -0700

    Querying Inside Stream

--- minjava/javaminibase/src/BigT/Stream.java
+++ minjava/javaminibase/src/BigT/Stream.java
@@ -5,8 +5,10 @@ import heap.HFBufMgrException;
 import iterator.*;
 import diskmgr.*;
 import heap.*;
+import java.util.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * This class will be similar to heap.Scan, however, will provide different
@@ -15,7 +17,7 @@ import java.io.IOException;
 public class Stream implements GlobalConst{
 
     /** in-core copy (pinned) of the same */
-    private bigt bigTable;
+    private bigt _bigTable;
     /** PageId of current directory page (which is itself an HFPage) */
     private PageId dirpageId = new PageId();
     /** pageId of bigT*/
@@ -36,7 +38,8 @@ public class Stream implements GlobalConst{
     private MID userrid = new MID();
     /** The amount of pages available for sorting*/
     int SORTPGNUM = 12;
-    /** the actual PageId of the data page with the current record */
+    /** Strings are set to use size 62 bytes, 2 extra bytes are added when setting hdr for maps, hence 64 */
+    private short RECLENGTH = 62;
 
 
     /**
@@ -48,16 +51,27 @@ public class Stream implements GlobalConst{
      * @param columnFilter
      * @param valueFilter
      */
-    Stream(bigt bigtable, int orderType, java.lang.String rowFilter, java.lang.String columnFilter, java.lang.String valueFilter){
+    Stream(bigt bigtable, int orderType, java.lang.String rowFilter, java.lang.String columnFilter, java.lang.String valueFilter)
+            throws InvalidTupleSizeException, IOException{
+        _bigTable = bigtable;
+        DataPageInfo dpinfo;
+        Tuple        rectuple = null;
+        Boolean      bst;
+
+        /** copy data about first directory page */
+
+        dirpageId.pid = _bigTable.hf._firstDirPageId.pid;
+        nextUserStatus = true;
+
         AttrType[] attrType = new AttrType[4];
         attrType[0] = new AttrType(AttrType.attrString);
         attrType[1] = new AttrType(AttrType.attrString);
         attrType[2] = new AttrType(AttrType.attrInteger);
         attrType[3] = new AttrType(AttrType.attrString);
         short[] attrSize = new short[3];
-        attrSize[0] = 64;
-        attrSize[1] = 64;
-        attrSize[2] = 64;
+        attrSize[0] = RECLENGTH;
+        attrSize[1] = RECLENGTH;
+        attrSize[2] = RECLENGTH;
         TupleOrder[] order = new TupleOrder[2];
         order[0] = new TupleOrder(TupleOrder.Ascending);
         order[1] = new TupleOrder(TupleOrder.Descending);
@@ -82,10 +96,13 @@ public class Stream implements GlobalConst{
         projlist[2] = new FldSpec(rel, 3);
         projlist[3] = new FldSpec(rel, 4);
 
+        // OutFilter for limiting results from BigTable search
+        CondExpr[] outFilter = queryFilter(rowFilter, columnFilter, valueFilter);
+
         FileScan fscan = null;
 
         try {
-            fscan = new FileScan(bigtable.name, attrType, attrSize, (short) 4, 4, projlist, null);
+            fscan = new FileScan(bigtable.Name, attrType, attrSize, (short) 4, 4, projlist, outFilter[0] == null ? null : outFilter );
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -112,8 +129,7 @@ public class Stream implements GlobalConst{
                     break;
                 case OrderType.type5:
                     //time stamp
-                    sort = new Sort(attrType, (short) 4, attrSize, fscan, 3, order[0], 4, SORTPGNUM);
-
+                    //sort = new Sort(attrType, (short) 4, attrSize, fscan, 3, order[0], 4, SORTPGNUM);
                     break;
                 default:
             }
@@ -136,40 +152,178 @@ public class Stream implements GlobalConst{
 
     }
 
-    //TODO need to set up for rowLabel, columnLabel, TS, value
-    private void Query_CondExpr(CondExpr[] expr, CondExpr[] expr2) {
+    /**
+     * Each filter can either be *, a single value, or a range [x,y]. This function takes the filters and determines
+     * What kind of operation each filter is then converts it into the appropriate CondExpr to be used with a FIleScan
+     * @param rowFilter
+     * @param columnFilter
+     * @param valueFilter
+     * @return the outFilter to be used with the FileScan
+     */
+    private CondExpr[] queryFilter(java.lang.String rowFilter, java.lang.String columnFilter, java.lang.String valueFilter) {
+        List<CondExpr> query = new ArrayList<CondExpr>();
+        // If RowFilter is a range, create a range expression
+        if(isRangeQuery(rowFilter)){
+            String[] bounds = getBounds(rowFilter);
+            for (CondExpr expr : rangeQuery(bounds[0], bounds[1], 0)){
+                query.add(expr);
+            }
+        }
+        // Else if rowFilter is an equality, create an identity expression
+        else if(isEqualityQuery(rowFilter)){
+            query.add(equalityQuery(rowFilter, 0));
+        }
+        // If ColumnFilter is a range, create a range expression
+        if(isRangeQuery(columnFilter)){
+            String[] bounds = getBounds(rowFilter);
+            for (CondExpr expr : rangeQuery(bounds[0], bounds[1], 1)){
+                query.add(expr);
+            }
+        }
+        // Else if columnFilter is an equality, create an identity expression
+        else if(isEqualityQuery(columnFilter)){
+            query.add(equalityQuery(columnFilter, 1));
+        }
 
-        expr[0].next  = null;
-        expr[0].op    = new AttrOperator(AttrOperator.aopEQ);
-        expr[0].type1 = new AttrType(AttrType.attrSymbol);
-        expr[0].type2 = new AttrType(AttrType.attrSymbol);
-        expr[0].operand1.symbol = new FldSpec (new RelSpec(RelSpec.outer),1);
-        expr[0].operand2.symbol = new FldSpec (new RelSpec(RelSpec.innerRel),1);
-
-        expr[1] = null;
-
-        expr2[0].next  = null;
-        expr2[0].op    = new AttrOperator(AttrOperator.aopEQ);
-        expr2[0].type1 = new AttrType(AttrType.attrSymbol);
-        expr2[0].type2 = new AttrType(AttrType.attrSymbol);
-        expr2[0].operand1.symbol = new FldSpec (new RelSpec(RelSpec.outer),2);
-        expr2[0].operand2.symbol = new FldSpec (new RelSpec(RelSpec.innerRel),1);
-
-        expr2[1].op   = new AttrOperator(AttrOperator.aopEQ);
-        expr2[1].next = null;
-        expr2[1].type1 = new AttrType(AttrType.attrSymbol);
-        expr2[1].type2 = new AttrType(AttrType.attrString);
-        expr2[1].operand1.symbol = new FldSpec (new RelSpec(RelSpec.innerRel),3);
-        expr2[1].operand2.string = "red";
-
-        expr2[2] = null;
+        // If ValueFilter is a range, create a range expression
+        if(isRangeQuery(valueFilter)){
+            String[] bounds = getBounds(rowFilter);
+            for (CondExpr expr : rangeQuery(bounds[0], bounds[1], 3)){
+                query.add(expr);
+            }
+        }
+        // Else if valueFilter is an equality, create an identity expression
+        else if(isEqualityQuery(valueFilter)){
+            query.add(equalityQuery(valueFilter, 3));
+        }
+        // convert results to the format required for scans
+        return ConvertToArray(query);
     }
 
     /**
+     * We Determine if a filter is a range by checking for the presence of the ',' character
+     * A range query would be in the format [x,y]
+     * @param filter
+     * @return
+     */
+    private boolean isRangeQuery(java.lang.String filter){
+        return filter.contains(",");
+    }
+
+    /**
+     * This filter check is context specific, it is always called after isRangeQuery(). In those cases, the only other
+     * possible filter under consideration is the single value. A single value will be anything not containing '*'
+     * @param filter
+     * @return
+     */
+    private boolean isEqualityQuery(java.lang.String filter){
+        return !filter.contains("*");
+    }
+
+    /**
+     * Parses a range and returns the upper and lower bounds
+     * @param filter [x,y] range
+     * @return
+     */
+    private String[] getBounds(java.lang.String filter){
+        return filter.replaceAll("(\\[|\\])", "").split(",");
+    }
+
+    /**
+     * Converts a List of queries into the format required by FileScan, CondExpr[].
+     * @param queries A List of Queries
+     * @return
+     */
+    private CondExpr[] ConvertToArray(List<CondExpr> queries){
+        // The CondExpr needs to terminate with a null CondExpr,
+        // Hence we take the query size + 1
+        CondExpr[] plan = new CondExpr[queries.size()+1];
+        for (int i = 0; i < queries.size(); i++) {
+            plan[i] = queries.get(i);
+        }
+        plan[queries.size()] = null;
+        return plan;
+    }
+
+    /**
+     * Generates a CondExpr for an equality operation
+     * @param rowFilter Single Value Filter
+     * @param offSet The OffSet of the map attribute under consideration
+     * @return
+     */
+    private CondExpr equalityQuery(java.lang.String rowFilter, int offSet){
+        // set up an equality expression
+        CondExpr expr = new CondExpr();
+        expr.op = new AttrOperator(AttrOperator.aopEQ);
+        expr.type1 = new AttrType(AttrType.attrSymbol);
+        expr.type2 = new AttrType(AttrType.attrString);
+        expr.operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer), offSet);
+        expr.operand2.string = rowFilter;
+        expr.next = null;
+        return expr;
+    }
+
+    /**
+     * Generates a set of CondExpr needed for a range search operation
+     * @param lowerBound String value of lower bound of range
+     * @param upperBound String value of upper bound of range
+     * @param offset The OffSet of the map attribute under consideration
+     * @return
+     */
+    private CondExpr[] rangeQuery(java.lang.String lowerBound, java.lang.String upperBound, int offset){
+        //range scan
+        CondExpr[] expr = new CondExpr[2];
+        expr[0] = new CondExpr();
+        expr[0].op = new AttrOperator(AttrOperator.aopGE);
+        expr[0].type1 = new AttrType(AttrType.attrSymbol);
+        expr[0].type2 = new AttrType(AttrType.attrString);
+        expr[0].operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer), offset);
+        expr[0].operand2.string = lowerBound;
+        expr[0].next = null;
+        expr[1] = new CondExpr();
+        expr[1].op = new AttrOperator(AttrOperator.aopLE);
+        expr[1].type1 = new AttrType(AttrType.attrSymbol);
+        expr[1].type2 = new AttrType(AttrType.attrString);
+        expr[1].operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer), offset);
+        expr[1].operand2.string = upperBound;
+        expr[1].next = null;
+        return expr;
+//        CondExpr [] expr2 = new CondExpr[3];
+//        expr2[0] = new CondExpr();
+//
+//
+//        expr2[0].next  = null;
+//        expr2[0].op    = new AttrOperator(AttrOperator.aopEQ);
+//        expr2[0].type1 = new AttrType(AttrType.attrSymbol);
+//
+//        expr2[0].operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer),1);
+//        expr2[0].type2 = new AttrType(AttrType.attrSymbol);
+//
+//        expr2[0].operand2.symbol = new FldSpec (new RelSpec(RelSpec.innerRel),1);
+//
+//        expr2[1] = new CondExpr();
+//        expr2[1].op   = new AttrOperator(AttrOperator.aopGT);
+//        expr2[1].next = null;
+//        expr2[1].type1 = new AttrType(AttrType.attrSymbol);
+//
+//        expr2[1].operand1.symbol = new FldSpec (new RelSpec(RelSpec.outer),4);
+//        expr2[1].type2 = new AttrType(AttrType.attrReal);
+//        expr2[1].operand2.real = (float)40.0;
+//
+//
+//        expr2[1].next = new CondExpr();
+//        expr2[1].next.op   = new AttrOperator(AttrOperator.aopLT);
+//        expr2[1].next.next = null;
+//        expr2[1].next.type1 = new AttrType(AttrType.attrSymbol); // rating
+//        expr2[1].next.operand1.symbol = new FldSpec ( new RelSpec(RelSpec.outer),3);
+//        expr2[1].next.type2 = new AttrType(AttrType.attrInteger);
+//        expr2[1].next.operand2.integer = 7;
+//
+//        expr2[2] = null;
+    }
+    /**
      * Closes the stream object.
      */
-    //TODO need to refactor to work with maps
-    // we still need to pin/unpin data from buffer manager (i think)
     void closestream(){
         if (bigTable != null) {
 
