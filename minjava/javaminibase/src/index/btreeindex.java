270541f30f2c9274204c0286b9c26a412dfa1526
Jonathan <jonreyes4@gmail.com>
2020-03-21 13:15:37 -0700

    BigDb, btreeindex.java, propTypes
    Update to BigDB CTOR, Modifications to btreeindex.java to work with BigDB, addition of PropertyTypes to global

--- minjava/javaminibase/src/index/btreeindex.java
+++ minjava/javaminibase/src/index/btreeindex.java
@@ -1,4 +1,4 @@
-package programs;
+package index;
 
 import btree.* ;
 import global.*;
@@ -9,19 +9,16 @@ import iterator.*;
 import java.io.IOException;
 import java.util.*;
 
-// get rowLabel (String), columnLabel (String), timestamp (Integer) from BigT.Map
-// code refers to IndexTest.java
-
 public class btreeindex{
-    short MAXLEN_S = 32;  //set max length of row label, column lable, values as 32
-    short MAXLEN_COMB_KEY = 64; // size of two strings
-    short MAXLEN_I = 4;	//set max length of timestamp, int as 4
+    static final short MAXLEN_S = 32;  //set max length of row label, column lable, values as 32
+    static final short MAXLEN_COMB_KEY = 64; // size of two strings
+    static final short MAXLEN_I = 4;	//set max length of timestamp, int as 4
 
     /**
      * @param propName The property name we'll build an index on: Row, Column, TimeStamp, Value
      * @return a btf file that will be assigned to the bigDB class and accessible globally
      */
-    public BTreeFile IndexForOneKey(java.lang.String propName){
+    public static BTreeFile IndexForOneKey(java.lang.String propName){
         //create an index file
         int keyType = propName.equalsIgnoreCase("timestamp") ? AttrType.attrInteger : AttrType.attrString;
         int keySize = propName.equalsIgnoreCase("timestamp") ? MAXLEN_I : MAXLEN_S;
@@ -41,7 +38,7 @@ public class btreeindex{
      * @param propName2 the name of the property we are indexing on that will be used to create a combined key
      * @return a btf file with a combined key setup that will be assigned to the bigDB class and accessible globally
      */
-    public BTreeFile IndexForCombinedKey(java.lang.String propName1, java.lang.String propName2) {
+    public static BTreeFile IndexForCombinedKey(java.lang.String propName1, java.lang.String propName2) {
         int keyType = AttrType.attrString;
         int keySize = MAXLEN_COMB_KEY;
         int delete_Fashion =1;
