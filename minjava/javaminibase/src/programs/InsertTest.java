71d0742e09e9a1a463750bc9d6a83b2a9f63cabc
Jon Reyes <jonreyes4@gmail.com>
2020-03-22 22:08:24 -0700

    insert test cleanup

--- minjava/javaminibase/src/programs/InsertTest.java
+++ minjava/javaminibase/src/programs/InsertTest.java
@@ -11,54 +11,18 @@ import java.io.*;
 public class InsertTest implements GlobalConst{
     public static void main(String[] args) throws Exception {
         boolean first = true;
-        String filepath = "./";
-        // String datafilename = args[0];
-        // int type = Integer.parseInt(args[1]);
-        // String bigTableName = args[2];
-
         String dbpath;
         String logpath;
-    
-        // dbpath = "/tmp/" + System.getProperty("user.name") + bigTableName + ".db";
-        // logpath = "/tmp/" + System.getProperty("user.name") + bigTableName + ".log";
-    
+
         SystemDefs sysdef = null;
-        // SystemDefs sysdef = new SystemDefs(dbpath, type, 8193, 100, "Clock");
-        
-        // String newdbpath;
-        // String newlogpath;
-        // String remove_logcmd;
-        // String remove_dbcmd;
-        // String remove_cmd = "/bin/rm -rf ";
-    
-        // newdbpath = dbpath;
-        // newlogpath = logpath;
-    
-        // remove_logcmd = remove_cmd + logpath;
-        // remove_dbcmd = remove_cmd + dbpath;
-    
-        // Commands here is very machine dependent. We assume
-        // user are on UNIX system here
-        // try {
-        //   Runtime.getRuntime().exec(remove_logcmd);
-        //   Runtime.getRuntime().exec(remove_dbcmd);
-        // } catch (IOException e) {
-        //   System.err.println("IO error: " + e);
-        // }
 
 
         Scanner scanner = new Scanner(System.in);
         try{
-            //String pattern = "\\*|(\\[.+\\])|\\w*";
             while(true){
                 System.out.println("Input batchinsert or query");
                 String line = scanner.nextLine();
                 String[] params = line.split(" ");
-                //Matcher m = Pattern.compile(pattern).matcher(line);
-                //List<String> params = New ArrayList<>();
-//                while(m.find()){
-//                    params.add(m.group(0));
-//                }
                 if(params[0].equalsIgnoreCase("batchinsert"))
                 {
                     String fname = params[1];
@@ -116,12 +80,5 @@ public class InsertTest implements GlobalConst{
         {
             System.out.println("exiting");
         }
-
-
-        
-
-        // batchInsert.insertTable(datafilename + ".");
-
-    
       }// end of main
 }
